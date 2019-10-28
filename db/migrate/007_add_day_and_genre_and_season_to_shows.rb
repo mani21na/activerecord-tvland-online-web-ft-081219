732b7fb7507a3cfbe70538612c265128d89ac690
Hanna Lee <mani21na@gmail.com>
2019-10-28 08:15:31 +0000

    Automatically backed up by Learn

--- db/migrate/007_add_day_and_genre_and_season_to_shows.rb
+++ db/migrate/007_add_day_and_genre_and_season_to_shows.rb
@@ -2,5 +2,6 @@ class AddDayAndGenreAndSeasonToShows < ActiveRecord::Migration[5.1]
   def change
     add_column :shows, :day, :string
     add_column :shows, :season, :string
+    add_column :shows, :genre, :string
   end
 end
